d63668e84a02fb7d8de4d62e2b1eabdadb43c4e0
Andrew Takao <takaoandrew@gmail.com>
2017-05-01 11:34:30 -0400

    initial commit

---
+++ app/src/main/java/com/andrewtakao/minnodemo/MainActivity.java
@@ -0,0 +1,74 @@
+package com.andrewtakao.minnodemo;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.GridView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+
+        GridView gridview = (GridView) findViewById(R.id.profile_grid);
+        gridview.setAdapter(new ProfilePictureAdapter(this));
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, FilterActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void seemore(View view) {
+        Intent intent = new Intent(this, SeeMoreActivity.class);
+        startActivity(intent);
+
+    }
+
+    public void echo(View view) {
+        Intent intent = new Intent(this, EchoMakingActivity.class);
+        startActivity(intent);
+    }
+
+    public void echolist(View view) {
+        Intent intent = new Intent(this, EchoListActivity.class);
+        startActivity(intent);
+    }
+}
---
+++ app/src/main/java/com/andrewtakao/minnodemo/ProfilePictureAdapter.java
@@ -0,0 +1,57 @@
+package com.andrewtakao.minnodemo;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+
+/**
+ * Created by andrewtakao on 4/18/17.
+ */
+
+public class ProfilePictureAdapter extends BaseAdapter {
+    private Context mContext;
+
+    // Constructor
+    public ProfilePictureAdapter(Context c) {
+        mContext = c;
+    }
+
+    public int getCount() {
+        return mThumbIds.length;
+    }
+
+    public Object getItem(int position) {
+        return null;
+    }
+
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    // create a new ImageView for each item referenced by the Adapter
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView;
+
+        if (convertView == null) {
+            imageView = new ImageView(mContext);
+            imageView.setLayoutParams(new GridView.LayoutParams(300, 300));
+            imageView.setScaleType(ImageView.ScaleType.CENTER_CROP);
+            imageView.setPadding(8, 8, 8, 8);
+        }
+        else
+        {
+            imageView = (ImageView) convertView;
+        }
+        imageView.setImageResource(mThumbIds[position]);
+        return imageView;
+    }
+
+    // Keep all Images in array
+    public Integer[] mThumbIds = {
+            R.drawable.profile4, R.drawable.profile3,
+            R.drawable.profile2, R.drawable.profile1
+    };
+}
